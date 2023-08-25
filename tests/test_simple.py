08fd616ef9003201b1ac83eab4eea911a6260447
Igor Nikolishen <igor.nikolishen@gmail.com>
2023-08-25 13:26:44 +0300

    add allure

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,14 +1,20 @@
+import allure
+
+@allure.story('hello')
 def test_1():
     assert True
 
+@allure.story('hello32')
 def test_2():
     assert True
 
+@allure.title('asdsadasd')
 def test_3():
     assert True
 
 def test_4():
     assert True
 
+@allure.title('hello213213')
 def test_5():
     assert True
