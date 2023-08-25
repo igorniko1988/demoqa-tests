9c088c856bba32e12dbc67c481e81895156a9aaa
Igor Nikolishen <igor.nikolishen@gmail.com>
2023-08-25 13:58:33 +0300

    add attachments

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,21 @@
-# import pytest
+import pytest
 # from selene.support.shared import browser
 # from faker import Faker
 # from demoqa_tests.data.users import User
 # from selenium.webdriver import FirefoxOptions, ChromeOptions
-# import allure
-# from allure import attach, attachment_type
-# import json
+import allure
+from allure import attach, attachment_type
+import json
 #
-# @pytest.fixture(autouse=True)
-# def attach():
-#     allure.attach("Text content", name="Text", attachment_type=attachment_type.TEXT)
-#
-#     # HTML
-#     allure.attach("<h1>Hello, world</h1>", name="Html", attachment_type=attachment_type.PNG)
-#
-#     # JSON
-#     allure.attach(json.dumps({"first": 1, "second": 2}), name="Json", attachment_type=attachment_type.JSON)
+@pytest.fixture(autouse=True)
+def attach():
+    allure.attach("Text content", name="Text", attachment_type=attachment_type.TEXT)
+
+    # HTML
+    allure.attach("<h1>Hello, world</h1>", name="Html", attachment_type=attachment_type.PNG)
+
+    # JSON
+    allure.attach(json.dumps({"first": 1, "second": 2}), name="Json", attachment_type=attachment_type.JSON)
 #
 # @pytest.fixture(autouse=True)
 # def browser_config():
